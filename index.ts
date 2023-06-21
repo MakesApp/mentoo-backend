1997192bd8aecd2c84406f0191acef7862bd93ae
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-06-21 16:51:34 +0300

    Restore

--- index.ts
+++ index.ts
@@ -17,10 +17,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-console.log(process.env.CLIENT_URL);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
