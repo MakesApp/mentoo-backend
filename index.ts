5f562793e398e4ccb428c90cd5ae4bc37dbe32e4
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-06-21 16:31:54 +0300

    ok

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL||'*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
