3e2d5d60a3ad1193922ea1a14f824e961a237172
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-06-22 15:17:18 +0300

    add socket io to main file

--- index.ts
+++ index.ts
@@ -4,9 +4,12 @@ import conversationRouter from "./src/components/conversations/conversation.rout
 import notificationRouter from "./src/components/notifications/notifications.routes";
 import placeRouter from './src/components/places/place.routes'
 import "./src/services/DB/mongoose";
-import "./src/services/socket/socket";
+// import "./src/services/socket/socket";
+import {Server} from 'socket.io'
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import Conversation, { Message } from "./src/components/conversations/conversation.model";
+import mongoose from "mongoose";
 dotenv.config();
 
 
@@ -40,6 +43,71 @@ app.use('/api',(_,res)=>{
   res.send('heelo')
 })
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+const server = app.listen(PORT, () =>
+  console.log(`Server started on ${PORT}`)
+);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  },
 });
+
+io.on('connection', (socket) => {
+  socket.on('join room', async(room, userId,partnerId) => {
+    socket.join(room);
+    let conversation = await Conversation.findOne({ room });
+    if (!conversation) {
+      conversation = new Conversation({ room, transcript: [], users: [userId, partnerId] });
+      await conversation.save();
+    } else {
+      if (!conversation.users.includes(userId)) {
+        conversation.users.push(userId);
+        await conversation.save();
+      }
+    }
+    if (conversation) {
+      socket.emit('chat history', conversation.transcript);
+    }
+  });
+
+  socket.on('chat message', async (msg, room) => {
+    let conversation = await Conversation.findOne({ room });
+    if (conversation) {
+      const msgObj = {
+        _id: new mongoose.Types.ObjectId(),
+        sender: new mongoose.Types.ObjectId(msg.sender),
+        message: msg.message,
+        createdAt: new Date(),
+      };
+      const newMessage = new Message(msgObj);
+      conversation.transcript.push(newMessage);
+      await conversation.save();
+      io.to(room).emit('chat message', msgObj);
+    }
+  });
+  
+  socket.on('messages seen', async (messageIds, room, userId) => {
+    try {
+      const conversation = await Conversation.findOne({ room });
+      if (conversation) {
+        messageIds.forEach(messageId => {
+          const message = conversation.transcript.find((msg) => msg._id.toString() === messageId);
+          if (message && !message.seenBy) {
+            message.seenBy = new mongoose.Types.ObjectId(userId);
+          }
+        });
+        await conversation.save();
+        io.to(room).emit('messages seen', messageIds);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    // Handle socket disconnection logic
+  });
+});
+
+
--- src/services/socket/socket.ts
+++ src/services/socket/socket.ts
@@ -1,97 +1,77 @@
 import express from "express";
-import server from "http";
+import { createServer } from "http";
 import { Server } from "socket.io";
 import Conversation, { Message } from "../../components/conversations/conversation.model";
+import mongoose from 'mongoose';
 
-import mongoose, { Schema } from 'mongoose'
 const app = express();
-const socketServer = server.createServer(app);
 
-console.log(`${process.env.CLIENT_URL}`);
+const httpServer = createServer(app);
+console.log(process.env.CLIENT_URL);
 
-const io = new Server(socketServer, {
+const io = new Server(httpServer, {
   cors: {
     origin: [`${process.env.CLIENT_URL}`],
     methods: ["GET", "POST"],
-    // credentials: true
   },
 });
 
-io.on('connection', (socket) => {
 
- 
+io.on('connection', (socket) => {
   socket.on('join room', async(room, userId,partnerId) => {
     socket.join(room);
-let conversation = await Conversation.findOne({ room });
-  if (!conversation) {
-    // If the conversation does not exist, create it
-    conversation = new Conversation({ room, transcript: [], users: [userId, partnerId] }); // Add partnerId to the users array
-    await conversation.save();
-  } else {
-    // If the conversation already exists and the user is not in it, add the user
-    if (!conversation.users.includes(userId)) {
-      conversation.users.push(userId);
+    let conversation = await Conversation.findOne({ room });
+    if (!conversation) {
+      conversation = new Conversation({ room, transcript: [], users: [userId, partnerId] });
       await conversation.save();
+    } else {
+      if (!conversation.users.includes(userId)) {
+        conversation.users.push(userId);
+        await conversation.save();
+      }
     }
-  }
-
-    // Fetch and send all messages from this room
-    // You don't need to fetch conversation again as it's already fetched above
     if (conversation) {
-      // Emit chat history to the user
       socket.emit('chat history', conversation.transcript);
     }
   });
 
-  // Handle a new chat message
   socket.on('chat message', async (msg, room) => {
     let conversation = await Conversation.findOne({ room });
-    if (conversation) { // Add this check to ensure conversation is not null
-      const msgObj={
+    if (conversation) {
+      const msgObj = {
         _id: new mongoose.Types.ObjectId(),
         sender: new mongoose.Types.ObjectId(msg.sender),
         message: msg.message,
-        createdAt: new Date(), // Add the createdAt timestamp
-
-      }
+        createdAt: new Date(),
+      };
       const newMessage = new Message(msgObj);
-      
-
-      // Add the new message to the transcript
       conversation.transcript.push(newMessage);
       await conversation.save();
-
-      // Emit the new message to all users in the room
       io.to(room).emit('chat message', msgObj);
     }
   });
   
   socket.on('messages seen', async (messageIds, room, userId) => {
-  try {
-    const conversation = await Conversation.findOne({ room });
-    if (conversation) {
-      messageIds.forEach(messageId => {
-        const message = conversation.transcript.find((msg) => msg._id.toString() === messageId);
-        if (message && !message.seenBy) {
-          // Set the seenBy field to the user's ID
-          message.seenBy = new mongoose.Types.ObjectId(userId);
-        }
-      });
-      await conversation.save();
-      // Emit the updated message to all users in the room
-      io.to(room).emit('messages seen', messageIds);
+    try {
+      const conversation = await Conversation.findOne({ room });
+      if (conversation) {
+        messageIds.forEach(messageId => {
+          const message = conversation.transcript.find((msg) => msg._id.toString() === messageId);
+          if (message && !message.seenBy) {
+            message.seenBy = new mongoose.Types.ObjectId(userId);
+          }
+        });
+        await conversation.save();
+        io.to(room).emit('messages seen', messageIds);
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
+  });
+
+  socket.on('disconnect', () => {
+    // Handle socket disconnection logic
+  });
 });
 
-
-    socket.on('disconnect', () => {
-    });
-});
-
-socketServer.listen(process.env.SOCKET_PORT||8080, () => {
-  console.log("socket server run on port ",process.env.SOCKET_PORT||8080);
-});
-
+export default httpServer;
