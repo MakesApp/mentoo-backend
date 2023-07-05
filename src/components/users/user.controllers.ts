2afd117ca849a3b74218c00f8b582646b2410b39
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-07-05 13:58:29 +0300

    try without options

--- src/components/users/user.controllers.ts
+++ src/components/users/user.controllers.ts
@@ -51,7 +51,7 @@ export const register = async (req: Request, res: Response) => {
     });
 
     // Set the token as a cookie
-    res.cookie("token", token, { httpOnly: true, secure: true });
+    res.cookie("token", token);
 
     res.status(201).json({ user: savedUser, message: "ההרשמה בוצעה בהצלחה" });
   } catch (error) {
@@ -90,8 +90,7 @@ console.log(email, password);
         });
         const modifiedUser = { role:user.role,...user.toObject(), password: undefined };
 
-     res.cookie("token", token, { httpOnly: true, secure: false ,sameSite: 'lax' // none, lax, or strict
-});
+     res.cookie("token", token});
 
         res.status(200).send({ message: "ההתחברות בוצעה בהצלחה", user:modifiedUser ,token});
       } else {
