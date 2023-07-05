d92ff412ed8d16173ade7f9cc6acf5b448046f84
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-07-05 14:03:08 +0300

    ok

--- src/components/users/user.controllers.ts
+++ src/components/users/user.controllers.ts
@@ -51,7 +51,7 @@ export const register = async (req: Request, res: Response) => {
     });
 
     // Set the token as a cookie
-    res.cookie("token", token);
+    res.cookie("token", token, { httpOnly: true, secure: true,sameSite:"none" });
 
     res.status(201).json({ user: savedUser, message: "ההרשמה בוצעה בהצלחה" });
   } catch (error) {
